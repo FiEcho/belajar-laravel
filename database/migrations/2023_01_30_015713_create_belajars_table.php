f08b7fe7b67f8bd7d33c5fd0051bb64fdd7d6f8c
Mr.Coman <maulanafitra1122@gmail.com>
2023-01-30 09:43:45 +0700

    menambahkan table di database

--- database/migrations/2023_01_30_015713_create_belajars_table.php
+++ database/migrations/2023_01_30_015713_create_belajars_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('belajars', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->integer('umur');
+            $table->text('alamat');
             $table->timestamps();
         });
     }
